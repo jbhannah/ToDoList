e2af8ef4185512579baa187d8142853f92f308e8
Jesse B. Hannah <jesse@jbhannah.net>
2013-12-28 14:51:38 -0700

    Toggle and display completion status of to-do items

--- ToDoList/TDLToDoListViewController.m
+++ ToDoList/TDLToDoListViewController.m
@@ -26,6 +26,11 @@
     return self;
 }
 
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue
+{
+
+}
+
 - (void)loadInitialData
 {
     TDLToDoItem *item1 = [[TDLToDoItem alloc] init];
@@ -81,6 +86,12 @@
     
     TDLToDoItem *toDoItem = [self.toDoItems objectAtIndex:indexPath.row];
     cell.textLabel.text = toDoItem.itemName;
+
+    if (toDoItem.completed) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
     
     return cell;
 }
@@ -136,9 +147,16 @@
 
  */
 
-- (IBAction)unwindToList:(UIStoryboardSegue *)segue
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+
+    TDLToDoItem *tappedItem = [self.toDoItems objectAtIndex:indexPath.row];
+    tappedItem.completed = !tappedItem.completed;
+
+    [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
 }
 
 @end
